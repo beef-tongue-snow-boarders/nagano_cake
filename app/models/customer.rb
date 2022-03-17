1cb3088b06eeceabe300e76987aed87dd01c22c4
matsu <98798507+matsuzaki40326@users.noreply.github.com>
2022-03-17 04:16:08 +0000

    [update]会員新規登録

--- app/models/customer.rb
+++ app/models/customer.rb
@@ -3,7 +3,7 @@ class Customer < ApplicationRecord
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
-  
+
   validates :last_name, presence: true
   validates :first_name, presence: true
   validates :last_name_kana, presence: true, format: { with: /\A[\p{katakana}　ー－&&[^ -~｡-ﾟ]]+\z/ }
@@ -11,5 +11,5 @@ class Customer < ApplicationRecord
   validates :postal_code, presence: true, format: { with: /\A\d{7}\z/ }
   validates :address, presence: true, format: { with: /\A[ぁ-んァ-ン一-龥]/ }
   validates :phone_number, presence: true, format: { with: /\A\d{10}\z/ }
-  
+
 end
