c725fe5723afa33a093b363dc5064001e2312bdf
matsu <98798507+matsuzaki40326@users.noreply.github.com>
2022-03-18 09:46:17 +0000

    [add]ジャンル検索

--- app/controllers/customer/items_controller.rb
+++ app/controllers/customer/items_controller.rb
@@ -1,11 +1,19 @@
 class Customer::ItemsController < ApplicationController
   def index
     @items = Item.page(params[:page])
+    @genres = Genre.all
+    if params[:id]
+      @genre = Genre.find(params[:id])
+      @items = @genre.items.page(params[:page])
+    else
+      @items = Item.page(params[:page])
+    end
   end
 
   def show
     @item = Item.find(params[:id])
     @cart_item = CartItem
+    @genres = Genre.all
   end
 
   private
