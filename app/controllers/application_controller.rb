433bbb38cfbb669a1f4194fdca283c68222c8e3a
matsu <98798507+matsuzaki40326@users.noreply.github.com>
2022-03-19 10:55:00 +0000

    [fix]遷移先変更

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,10 +1,32 @@
 class ApplicationController < ActionController::Base
-  devise_group :customer_or_admin, contains: [:customer, :admin]
-  before_action :authenticate_customer_or_admin!,except: [:top, :about, :index, :show]
-  
+  devise_group :admin_or_customer, contains: [:admin, :customer]
+  before_action :authenticate_admin_or_customer!,except: [:top, :about, :index, :show]
+
   before_action :item_search
   def item_search
     @search_item = Item.ransack(params[:q])
     @results = @search_item.result
   end
+
+  private
+
+  def after_sign_in_path_for(resource_or_scope)
+    if resource_or_scope.is_a?(Admin)
+      admin_path
+    else
+      current_customer
+    end
+  end
+
+  def after_sign_out_path_for(resource_or_scope)
+    if resource_or_scope == :customer
+      root_path
+    elsif resource_or_scope == :admin
+        new_admin_session_path
+    else
+  		  root_path
+    end
+  end
+
+
 end
