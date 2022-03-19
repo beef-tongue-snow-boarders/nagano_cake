64c7a028cf44f530e82bbabb4c6d3ac8fb362bf7
matsu <98798507+matsuzaki40326@users.noreply.github.com>
2022-03-19 09:10:34 +0000

    [add]アクセス権限

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,6 @@
 class ApplicationController < ActionController::Base
+
+  devise_group :customer_or_admin, contains: [:customer, :admin]
+  before_action :authenticate_customer_or_admin!,except: [:top, :about, :index, :show]
+
 end
